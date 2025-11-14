c78eb09e945cf535e5e6e5a870256d3c3b39d9d4
JudiTeller <praisethesun@firelink-shrine.de>
2025-11-14 14:22:17 +0100

    added Player script to offer inventory functions

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    
+    
+    // INVENTROY 
+    public bool hasItem(Item item)
+    {
+        return false;
+    }
+    
+    public bool addItem(Item item)
+    {
+        return false;
+    }
+    
+    public bool removeItem(Item item)
+    {
+        return false;
+    }
+    
+    public List<Item> getItems()
+    {
+        return new List<Item>();
+    }
+}
