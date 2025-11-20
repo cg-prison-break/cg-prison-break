c35a1aec67a5aa27d94c409fbeb368502ee00bb8
JudiTeller <praisethesun@firelink-shrine.de>
2025-11-20 12:48:55 +0100

    changed description comments

--- Assets/Scripts/Objects/Interactables/Item.cs
+++ Assets/Scripts/Objects/Interactables/Item.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class ItemData : ScriptableObject
 {
-    public string id; // don't know if necessary
-    public string itemName;
-    public Sprite icon;
-    public GameObject prefab;
+    public string id; // Currently unused
+    public string itemName; 
+    public Sprite icon; // Sprite to display in UI, May be changed dpending on the inventory UI implementation
+    public GameObject prefab; // The corresponding prefab NOT scene object
 }
