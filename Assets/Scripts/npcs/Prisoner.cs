6cefc3b2319c6038f347fa5943d2950cbd6cd628
tbollmeyer <89338555+tbollmeyer@users.noreply.github.com>
2025-11-30 20:43:12 +0100

    Use prisoner variants

--- Assets/Scripts/npcs/Prisoner.cs
+++ Assets/Scripts/npcs/Prisoner.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class Prisoner : NPC
 {
+    private readonly string[] variants = new string[] { "prisoner", "prisoner1", "prisoner5", "prisoner6" };
+
+    void Awake()
+    {
+        // select a random variant
+        var rnd = new System.Random();
+        var variant = variants[rnd.Next(variants.Length)];
+
+        foreach (Transform child in transform)
+        {
+            bool shouldBeActive = child.name == variant;
+            child.gameObject.SetActive(shouldBeActive);
+        }
+    }
+
     protected override void Start()
     {
         base.Start();
