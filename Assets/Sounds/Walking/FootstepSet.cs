d24e263ba8873cbd1e73c338d0267bddbb81d0b1
Aremju <juliusarendt@icloud.com>
2025-11-30 23:02:47 +0100

    add sounds to player movement

---
+++ Assets/Sounds/Walking/FootstepSet.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Sounds.Walking
+{
+    [System.Serializable]
+    public class FootstepSet
+    {
+        public string surfaceTag;
+        public AudioClip[] footsteps;
+    }
+}
---
+++ Assets/Sounds/Walking/JumpSet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Sounds.Walking
+{
+    [System.Serializable]
+    public class JumpSet
+    {
+        public string surfaceTag;
+        public AudioClip[] jumpSounds;
+        public AudioClip[] landingSounds;
+    }
+}
