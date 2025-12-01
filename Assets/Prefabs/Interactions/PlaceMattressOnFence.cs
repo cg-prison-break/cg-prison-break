0f2172f83b40b9cff1053b0d4ef79fa76077f33a
Aremju <juliusarendt@icloud.com>
2025-12-01 11:54:53 +0100

    fix navmesh surface and fence feature

--- Assets/Prefabs/Interactions/PlaceMattressOnFence.cs
+++ Assets/Prefabs/Interactions/PlaceMattressOnFence.cs
@@ -6,6 +6,7 @@ namespace Prefabs.Interactions
     public class PlaceMattressOnFence : MonoBehaviour, IInteractable
     {
         public GameObject fenceWithMattressPrefab;
+        public GameObject parent;
 
         public string InteractionPrompt
         {
@@ -15,8 +16,8 @@ namespace Prefabs.Interactions
 
         public void Interact(Player interactor)
         {
-            Instantiate(fenceWithMattressPrefab, transform.position, transform.rotation);
-            Destroy(gameObject);
+            Instantiate(fenceWithMattressPrefab, parent.transform.position, parent.transform.rotation);
+            Destroy(parent);
         }
     }
 }
