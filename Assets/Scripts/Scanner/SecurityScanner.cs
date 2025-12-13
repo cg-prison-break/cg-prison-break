75c69792f03b7d06fc5fa60ba0dbf6b0417ca307
Aremju <juliusarendt@icloud.com>
2025-12-13 23:36:12 +0100

    add initial security scanner prefab

---
+++ Assets/Scripts/Scanner/SecurityScanner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scanner
+{
+    public class SecurityScanner : MonoBehaviour
+    {
+        [SerializeField] private List<ItemData> illegalItems;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+            var player = other.GetComponent<Player>();
+            if (!player.HasOneOf(illegalItems)) return;
+            var audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+            NPCEventManager.NotifyNPCsAboutSuspiciousAction(player.transform.position);
+        }
+    }
+}
