177c368b7131f83dbb92f0074abb8e1f5efab186
Mark Mödeker <81245080+mmoedeker@users.noreply.github.com>
2025-12-14 13:39:35 +0100

    define endingscene for game over or victory

---
+++ Assets/Scripts/ui/EndingType.cs
@@ -0,0 +1,10 @@
+public enum EndingType { Good, Bad }
+
+public static class EndingContext
+{
+    public static EndingType NextEnding = EndingType.Good;
+}
+
+// When you trigger the ending do the following:
+// EndingContext.NextEnding = EndingType.Bad;
+// SceneManager.LoadScene("EndingScene");
