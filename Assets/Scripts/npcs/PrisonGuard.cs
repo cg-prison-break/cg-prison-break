591c26b3d62111f7e1becad5e4fa2de1af3e4810
tbollmeyer <89338555+tbollmeyer@users.noreply.github.com>
2026-01-01 12:33:29 +0100

    Remove prisonguard attention

--- Assets/Scripts/npcs/PrisonGuard.cs
+++ Assets/Scripts/npcs/PrisonGuard.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 
 public class PrisonGuard : NPC
 {
-    [Range(0.1f, 1.0f)]
-    public float attention = 0.5f;
-    public float maxAttentionRange = 20.0f;
-
     [Header("Sounds")]
     public AudioSource audioSource;
     public NPCInteractionSoundSet soundSet;
+
+    private readonly float suspiciousRange = 15f;
 
     protected override void Start()
     {
@@ -18,8 +16,6 @@ public class PrisonGuard : NPC
         NPCEventManager.OnSuspiciousActionEvent += HandleSuspiciousEvent;
         NPCEventManager.OnResetSuspiciousPrisonGuardsEvent += ResetToRandomMovement;
         NPCEventManager.OnAlertAllPrisonGuardsEvent += HandleAlertAllEvent;
-
-        attention = Random.Range(0.4f, 1.0f);
 
         ChangeState(new RandomMovementState());
     }
@@ -49,7 +45,7 @@ public class PrisonGuard : NPC
             // check if suspicious event location is nearby
             float distance = Vector3.Distance(transform.position, location);
 
-            if (distance < attention * maxAttentionRange)
+            if (distance < suspiciousRange)
             {
                 ChangeState(new SuspiciousState(location));
             }
