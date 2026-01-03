eb825004affa798986c63e2fab243c2a93c32fb8
Mark Mödeker <81245080+mmoedeker@users.noreply.github.com>
2026-01-03 20:18:21 +0100

    ensure exchangeability of scenes

--- Assets/Prefabs/StartAnimation/WalkPivotSceneManager.cs
+++ Assets/Prefabs/StartAnimation/WalkPivotSceneManager.cs
@@ -13,7 +13,7 @@ namespace Prefabs.StartAnimation
         {
             gameData.animationPlayed = true;
             loadingSceneIndicator.SetActive(true);
-            SceneManager.LoadScene(GameScene.MainSceneDecorated);
+            SceneManager.LoadScene(GameScene.MainScene);
         }
     }
 }
--- Assets/Scenes/GameScene.cs
+++ Assets/Scenes/GameScene.cs
@@ -2,8 +2,7 @@
 public class GameScene
 {
     public static string StartScene => "StartAnimation";
-    public static string MainScene => "MainScene";
-    public static string MainSceneDecorated => "MainSceneDecorated";
+    public static string MainScene => "MainSceneDecorated";
     public static string MainMenu => "MainMenu";
     public static string Ending => "EndingScene";
 }
