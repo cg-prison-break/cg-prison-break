03fed2b40e0bd062a34a5ec8be12163a4ce96997
Aremju <juliusarendt@icloud.com>
2026-01-04 23:13:17 +0100

    interactable switch for access card

--- Assets/Prefabs/Items/AccessCard/AccessCard.cs
+++ Assets/Prefabs/Items/AccessCard/AccessCard.cs
@@ -7,6 +7,7 @@ public class AccessCard : MonoBehaviour, IInteractableItem
 {
     [Header("Data")]
     [SerializeField] private ItemData _itemData;
+    [SerializeField] private GameData gameData;
 
     public ItemData itemData
     {
@@ -37,6 +38,38 @@ public class AccessCard : MonoBehaviour, IInteractableItem
     {
         bool pickedUp = player.AddItem(itemData);
         if (pickedUp) Destroy(gameObject);
+    }
+    
+    private void FixedUpdate()
+    {
+        var player = PlayerRegistry.Player;
+        // check if the player is near to the object, then set the layer of the object and all of its children to "Interactable"
+        if (Vector3.Distance(transform.position, player.transform.position) < 5.5f)
+        {
+            SetLayerRecursively(gameObject, LayerMask.NameToLayer(GetInteractableLayerName()));
+            if (!gameData.playWithInteractableShader)
+            {
+                // todo: implement logic for making lights on when shader is disabled
+            }
+        }
+        else
+        {
+            SetLayerRecursively(gameObject, LayerMask.NameToLayer("Default"));
+        }
+    }
+        
+    private void SetLayerRecursively(GameObject obj, int layer)
+    {
+        obj.layer = layer;
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, layer);
+        }
+    }
+        
+    private string GetInteractableLayerName()
+    {
+        return gameData.playWithInteractableShader ? "Interactable" : "InteractableNoOutline";
     }
     
 }
