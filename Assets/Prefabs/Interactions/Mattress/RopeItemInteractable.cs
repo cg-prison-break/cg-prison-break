866aa94530e6a8f72d41d8aedceaccc1b68372bc
Aremju <juliusarendt@icloud.com>
2026-01-04 23:17:42 +0100

    interactable switch for rope

--- Assets/Prefabs/Interactions/Mattress/RopeItemInteractable.cs
+++ Assets/Prefabs/Interactions/Mattress/RopeItemInteractable.cs
@@ -7,6 +7,7 @@ namespace Prefabs.Interactions.Mattress
     {
         [Header("Data")]
         [SerializeField] private ItemData _itemData;
+        [SerializeField] private GameData gameData;
 
         public ItemData itemData
         {
@@ -24,6 +25,38 @@ namespace Prefabs.Interactions.Mattress
         {
             var pickedUp = player.AddItem(itemData);
             if (pickedUp) Destroy(gameObject);
+        }
+        
+        private void FixedUpdate()
+        {
+            var player = PlayerRegistry.Player;
+            // check if the player is near to the object, then set the layer of the object and all of its children to "Interactable"
+            if (Vector3.Distance(transform.position, player.transform.position) < 5.5f)
+            {
+                SetLayerRecursively(gameObject, LayerMask.NameToLayer(GetInteractableLayerName()));
+                if (!gameData.playWithInteractableShader)
+                {
+                    // todo: implement logic for making lights on when shader is disabled
+                }
+            }
+            else
+            {
+                SetLayerRecursively(gameObject, LayerMask.NameToLayer("Default"));
+            }
+        }
+        
+        private void SetLayerRecursively(GameObject obj, int layer)
+        {
+            obj.layer = layer;
+            foreach (Transform child in obj.transform)
+            {
+                SetLayerRecursively(child.gameObject, layer);
+            }
+        }
+        
+        private string GetInteractableLayerName()
+        {
+            return gameData.playWithInteractableShader ? "Interactable" : "InteractableNoOutline";
         }
     }
 }
